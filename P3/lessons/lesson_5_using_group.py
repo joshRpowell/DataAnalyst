9910bf8a7b6ab6b9ce8cec3b46a072ebf0ceda63
Josh Powell <josh@innovativeops.co>
2015-11-04 15:15:02 -0500

    add lessons 5 and 6

--- P3/lessons/lesson_5_using_group.py
+++ P3/lessons/lesson_5_using_group.py
@@ -31,9 +31,10 @@ def get_db(db_name):
 
 def make_pipeline():
     # complete the aggregation pipeline
-    pipeline = [{"$group": {"_id": "$source",
-                 "count": {"$sum": 1}}},
-                {"$sort": {"count": -1}}]
+    pipeline = [
+        {"$group": {"_id": "$source", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
     return pipeline
 
 def tweet_sources(db, pipeline):
